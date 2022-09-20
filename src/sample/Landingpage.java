82c071982b0d95468492f1f9d78fac2503f312fb
SANCHAI376 <105773126+SANCHAI376@users.noreply.github.com>
2022-09-20 14:45:01 +0530

    Update Landingpage.java

--- src/sample/Landingpage.java
+++ src/sample/Landingpage.java
@@ -1,16 +1,19 @@
 package sample;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+
+
 public class Landingpage {
     WebDriver driver;
     public Landingpage(WebDriver driver) {
         this.driver =driver;
         PageFactory.initElements(driver, this);
     }
+    
     @FindBy(id = "username")
     WebElement  username;
     
@@ -23,60 +26,50 @@ public class Landingpage {
     @FindBy(xpath="//button[@type='submit']")
     WebElement Select;
     
-    public void character3(String name, String password) {
+    public void loginpage(String name, String password) throws InterruptedException {
         Select.click();
         username.sendKeys(name);
         passwords.sendKeys(password);
         driver.findElement(By.xpath("//a[@type='submit']")).click();
+        Thread.sleep(1000);
+        
+        driver.quit();
+    }
+    public void Wronguser(String name, String password) throws InterruptedException {
+        Select.click();
+        username.sendKeys(name);
+        passwords.sendKeys(password);
+        driver.findElement(By.xpath("//a[@type='submit']")).click();
+        Thread.sleep(1000);
+        driver.quit();
+    }
+    public void Wrong(String name, String password) throws InterruptedException {
+        Select.click();
+        username.sendKeys(name);
+        passwords.sendKeys(password);
+        driver.findElement(By.xpath("//a[@type='submit']")).click();
+        Thread.sleep(1000);
+        driver.quit();
+    }
+    public void blank(String name, String password) throws InterruptedException {
+        Select.click();
+        username.sendKeys(name);
+        passwords.sendKeys(password);
+        driver.findElement(By.xpath("//a[@type='submit']")).click();
+        Thread.sleep(1000);
+        driver.quit();
+    }
+    public void invalid(String name, String password) throws InterruptedException {
+        Select.click();
+        username.sendKeys(name);
+        passwords.sendKeys(password);
+        driver.findElement(By.xpath("//a[@type='submit']")).click();
+        Thread.sleep(1000);
+        driver.quit();
     }
     
-    public void character20(String name, String password) {
-        Select.click();
-        username.sendKeys(name);
-        passwords.sendKeys(password);
-        driver.findElement(By.xpath("//a[@type='submit']")).click();
     
-    }
-    public void character2(String name,String password) {
-        Select.click();
-        username.sendKeys(name);
-        passwords.sendKeys(password);
-        driver.findElement(By.xpath("//a[@type='submit']")).click();
-    
-    }
-    
-    public void character21(String name,String password) {
-        Select.click();
-        username.sendKeys(name);
-        passwords.sendKeys(password);
-        driver.findElement(By.xpath("//a[@type='submit']")).click();
-    }
-    
-    public void validemail(String name,String password) {
-        Select.click();
-        username.sendKeys(name);
-        passwords.sendKeys(password);
-        driver.findElement(By.xpath("//a[@type='submit']")).click();
-    }
-    public void unregisteremail(String name,String password) {
-        Select.click();
-        username.sendKeys(name);
-        passwords.sendKeys(password);
-        driver.findElement(By.xpath("//a[@type='submit']")).click();
-    }
-    public void validdetails(String name,String password) {
-        Select.click();
-        username.sendKeys(name);
-        passwords.sendKeys(password);
-        driver.findElement(By.xpath("//a[@type='submit']")).click();
-    }
-    public void pass2(String name,String password) {
-        Select.click();
-        username.sendKeys(name);
-        passwords.sendKeys(password);
-        driver.findElement(By.xpath("//a[@type='submit']")).click();
-    }
     public void goTo() {
-        driver.get(" https://mobileworld.banyanpro.com/");
+    	driver.get("https://mobileworld.banyanpro.com/");
     }
 }
